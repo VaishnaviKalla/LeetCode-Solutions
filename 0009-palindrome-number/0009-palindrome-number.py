c827edf62b048eac9ef8d014f96abeed3efe8653
VaishnaviKalla <vaishnavikalla399@gmail.com>
2026-04-18 14:33:48 +0530

    Time: 25 ms (8.74%), Space: 12.4 MB (25.87%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0:
+            return False
+
+        x = str(x)
+        n = len(x)
+        for i in range(n//2):
+            if x[i] != x[n-i-1]:
+                return False
+    
+        return True
+        
